dc0e79aa92c8d27eb5cbf689a2e150999ae34057
sudheshna reddy <sudheshna.jinna@zemosolabs.com>
2022-02-07 00:56:32 +0530

    Assignment 9

---
+++ Assignment9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Assignment9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = nextLine();
+        if (Pattern.matches("[A-Z].*[.]", input))
+            ;
+    }
+}
